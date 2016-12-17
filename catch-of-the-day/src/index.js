c14a9c6ebe9f61c6d611bf1959e76a72abc97a8c
George <george@weiksner.com>
2016-12-17 19:52:20 +0000

    Lesson 09

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,23 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+import { BroswerRouter, Match, Miss} from 'react-router'
+
+import './css/style.css';
+import App from './components/App';
+import NotFound from './components/NotFound'
+
+import StorePicker from './components/StorePicker';
+
+const Root = () => {
+    return (
+        <BroswerRouter>
+            <div>
+                <Match exactly pattern="/" component={StorePicker} />
+                <Match pattern="/store/:storeId" component={App} />
+                <Miss component={NotFound} />
+            </div>
+        </BrowserRouter>
+        )
+}
+
+render(<App/>, document.querySelector('#main'));
